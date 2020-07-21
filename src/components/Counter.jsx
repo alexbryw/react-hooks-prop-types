a97492aed9aea35f92e8767878cb8a91e5122b82
alexbryw <alexbryw@gmail.com>
2020-07-21 20:08:58 +0200

    prevCounter tested.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,23 @@ import React, {useState} from 'react'
 import './Counter.css'
 
 function Counter({step = 1}){
+    // const initCounter = 0
     const [counter, setCounter] = useState(0)
+
+    const addFive = () => {
+        for(let i = 0; i < 5; i++){
+            setCounter(prevCounter =>{
+                console.log(prevCounter)
+                return (
+                    prevCounter + 1
+                )
+            })
+        }
+    }
     return(
         <div className="Counter">
             <h3>Count: {counter}</h3>
-            <button onClick={() => setCounter(counter + step)}>+{step}</button>
+            <button onClick={addFive}>+{step}</button>
         </div>
     )
 }
