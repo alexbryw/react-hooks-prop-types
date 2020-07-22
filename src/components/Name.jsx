e9012bcad850a5238dfa5d5ac5197fceb1f0fa6a
alexbryw <alexbryw@gmail.com>
2020-07-22 10:36:46 +0200

    Updated Name component and css.

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
+import './Name.css'
 
 export default function Name(){
-    const [name, setName] = useState({fName: "",lName: ""})
+    const [name, setName] = useState({firstName: "",lastName: ""})
 
     return(
-        <div>
-            <input type="text" onChange={e => setName({lName: e.target.value})} />
+        <div className="Name">
+            <h4>First name:</h4>
+            <input type="text" onChange={e => setName({...name ,firstName: e.target.value})} />
+            <h4>Last name:</h4>
+            <input type="text" name="" id="" onChange={e => setName({...name, lastName: e.target.value})} />
             <h3>{JSON.stringify(name)}</h3>
         </div>
     )
